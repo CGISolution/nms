8f717d7525ae3d8ba655edf49209a8a954d38085
William Gallios <williamgallios@gmail.com>
2014-08-21 18:52:48 -0400

    dev

--- app/app.js
+++ app/app.js
@@ -1,11 +1,13 @@
 var app = angular.module('nms', ['ngRoute', 'ui.bootstrap']);
 
-app.config(function ($routeProvider, $locationProvider){
+app.config(function ($routeProvider, $locationProvider, $logProvider){
+
+    $logProvider.debugEnabled(true);
     $locationProvider.html5Mode(true);
 
     $routeProvider.when('/', {
         templateUrl: 'views/login.html',
     }).when('/login', {
     }).otherwise({ redirectTo: '/' });
-    
+
 });
--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,11 +1,14 @@
 
-function index ($scope)
+function index ($scope, $log)
 {
-    $scope.alerts = [];
-    //$scope.alerts = [{ type: 'warning', msg: 'Test' }];
+    //$scope.alerts = [];
+    $scope.alerts = [{ type: 'warning', msg: 'Test' }];
+
+    $log('Test log');
 
     $scope.removeAlert = function (index)
     {
         $scope.alerts.splice(index, 1);
     };
+
 }
